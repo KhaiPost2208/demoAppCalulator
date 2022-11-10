7638ddf52442aa3b124418a003475aa995378a64
Khai Dang <nguyendangkhai.telecom@gmail.com>
2022-11-10 17:46:40 +0700

    calulator1

---
+++ android/app/src/main/kotlin/com/example/calalutar_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calalutar_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+import 'dart:math';
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int Sodau = 0;
+  int Sosau = 0;
+  String opp = "" ;
+  String ketqua ="0";
+  String Hienthi ="" ;
+  buttonPress(String Click){
+    if (Click == "AC"){
+      ketqua = "";
+      Sodau = 0;
+      Sosau = 0;
+      Hienthi = "";
+    }
+    else if (Click == "+" || Click == "-" || Click == "X" || Click == "/" || Click == ".") {
+      Sodau = int.parse(Hienthi);
+      ketqua = "";
+      opp = Click;
+    }
+    else if (opp == "."){
+      if (ketqua.contains(".")){
+        print("No");
+        return;
+      }
+      else{
+        ketqua = ketqua + Click;
+      }
+    }
+    else if (Click == "=") {
+      Sosau = int.parse(Hienthi);
+      if(opp == "+"){
+        ketqua = (Sodau + Sosau).toString();
+      }
+      else if (opp == "-") {
+        ketqua = (Sodau - Sosau).toString();
+      }
+      else if (opp == "X") {
+        ketqua = (Sodau * Sosau).toString();
+      }
+      else if (opp == "/") {
+        ketqua = (Sodau / Sosau).toString();
+      }
+      else if (opp == "^2") {
+        ketqua = (Sodau * Sodau).toString();
+      }
+      else if (opp == "Căn") {
+        ketqua = (sqrt(Sodau)).toString();
+      }
+    }
+    else {
+      ketqua = int.parse(Hienthi + Click).toString();
+    }
+
+    setState(() {
+      Hienthi = ketqua;
+    });
+
+  }
+
+  Widget Button(String Click, Color Clickcolor, Color txtcolor){
+    return Container(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: CircleBorder(),
+          fixedSize: Size(80, 80),
+          primary: Clickcolor,
+        ),
+        onPressed: () => buttonPress(Click),
+        child: Text(Click,
+          style: TextStyle(fontSize: 25, color: txtcolor),
+
+        ),
+
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("Máy Tính"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(padding: EdgeInsets.all(10),
+                  child: Text(Hienthi,
+                    //textAlign: TextAlign.right,
+                    style: TextStyle(fontSize: 40, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Button("AC", Colors.grey, Colors.black),
+                Button("Căn", Colors.grey, Colors.black),
+                Button("^2", Colors.grey, Colors.black),
+                Button("/", Colors.orangeAccent, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Button("7", Colors.grey, Colors.black),
+                Button("8", Colors.grey, Colors.black),
+                Button("9", Colors.grey, Colors.black),
+                Button("X", Colors.orangeAccent, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Button("4", Colors.grey, Colors.black),
+                Button("5", Colors.grey, Colors.black),
+                Button("6", Colors.grey, Colors.black),
+                Button("-", Colors.orangeAccent, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Button("1", Colors.grey, Colors.black),
+                Button("2", Colors.grey, Colors.black),
+                Button("3", Colors.grey, Colors.black),
+                Button("+", Colors.orangeAccent, Colors.white),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Button("0", Colors.grey, Colors.black),
+                Button(".", Colors.grey, Colors.black),
+                Button("=", Colors.orangeAccent, Colors.white),
+              ],
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
